6b7c7f4fa5fe9d1a9861855ae187330b4f90b859
shouziadeeb <shouziadeeb123@gamil.com>
2025-09-01 11:17:27 +0530

    update db

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,9 +1,7 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "school_management",
+  uri: process.env.DATABASE_URL,
 });
+
 export default db;
